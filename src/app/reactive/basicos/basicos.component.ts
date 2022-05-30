7328ee5af014fb457a7c697d6c600edf1cd1994c
EmmanuelR12 <erangel@tecnolika.com>
2022-05-30 16:55:47 -0500

    reactivos

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormControl, FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
   templateUrl: './basicos.component.html',
   styleUrls: ['./basicos.component.css']
 })
-export class BasicosComponent implements OnInit {
+export class BasicosComponent  {
+  //miFormulario: FormGroup = new FormGroup({
+    //'nombre': new FormControl('Rtx 4080ti'),
+    //'precio': new FormControl(1500),
+    //'existencias': new FormControl(5),
+  //})
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  constructor(private formbuilder:FormBuilder) { }
+  
+  miFormulario: FormGroup = this.formbuilder.group({
+    nombre: ['Rtx 4080ti'],
+    precio:[0],
+    existencias:[0],
+  })
 
 }
